43c46454d2d823e4153bfec1ffca227993ec37a0
flopezcardozo <flopezcardozo@gmail.com>
2023-11-11 10:31:00 -0300

    Firs Commit Jenkinsfile

---
+++ Build.groovy
@@ -0,0 +1,88 @@
+pipeline{
+    agent {
+         label any
+    }
+    environment {
+       REPOURL = "https://github.com/Minervatechuy/Admin-API.git"
+       BRANCH = "main"
+    }
+
+    stages{
+        stage('Checkout'){
+            steps{
+                checkout([$class: 'GitSCM', branches: [[name: '${BRANCH}']], extensions: [], userRemoteConfigs: [[url: '${REPOURL}']]])
+            }
+        }
+        stage('Build artifact'){
+            agent {
+                label 'maven'
+            }
+            steps{
+                sh '''
+                    #mvn clean install
+                    /opt/apache-maven-3.6.3/bin/mvn package
+                '''
+            }
+        }
+        stage('Upload to nexus'){
+            agent {
+                label 'maven'
+            }
+            steps{
+                script{
+                    pom = readMavenPom(file: 'pom.xml')
+                    filesByGlob = findFiles(glob: "target/*.${pom.packaging}")
+                    echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
+                    artifactPath = filesByGlob[0].path
+                    artifactExists = fileExists artifactPath
+
+                    if(artifactExists) {
+                        echo "*** File: ${artifactPath}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}";
+                        
+                        nexusArtifactUploader(
+                            nexusVersion: NEXUS_VERSION,
+                            protocol: NEXUS_PROTOCOL,
+                            nexusUrl: NEXUS_URL,
+                            groupId: pom.groupId,
+                            version: pom.version,
+                            repository: NEXUS_REPOSITORY,
+                            credentialsId: NEXUS_CREDENTIAL_ID,
+                            artifacts: [
+                                // Artifact generated such as .jar, .ear and .war files.
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: artifactPath,
+                                type: pom.packaging],
+
+                                // Lets upload the pom.xml file for additional information for Transitive dependencies
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: "pom.xml",
+                                type: "pom"]
+                            ]
+                        );
+
+                    } else {
+                        error "*** File: ${artifactPath}, could not be found";
+                    }
+                }
+
+            }
+        } //fin stage upload
+        
+        
+        stage("Post") {
+            agent {
+                label 'docker'
+            }
+            steps {
+                sh '''
+                    pwd
+                    echo "Clean up workfolder"
+                    rm -Rf *
+                '''
+            }
+        } //fin stage post
+        
+    }
+}
