1877abd203d711bf5581c0fc84c4418c02741db8
flopezcardozo <flopezcardozo@gmail.com>
2023-11-15 19:38:16 -0300

    fix pipeline 15-11-08

--- Build.groovy
+++ Build.groovy
@@ -61,7 +61,7 @@ pipeline{
                     echo "healthcheck api_python"                    
                     #curl -v -k http://172.31.35.158:5000 | grep "HTTP/1.0"
                     API_TEST_RESULT = $(curl -s -o /dev/null -I -w "%{http_code}" http://172.31.35.158:5000)
-                    echo '${API_TEST_RESULT}'
+                    echo "${API_TEST_RESULT}"
                 '''
             }
         } //fin stage post
