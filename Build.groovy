f0c303cdcb7addbcd5efbf4a7aea356c3885f03c
flopezcardozo <flopezcardozo@gmail.com>
2023-11-16 16:00:57 -0300

    Fix jenkins api pipeline 16-11-01

--- Build.groovy
+++ Build.groovy
@@ -61,8 +61,22 @@ pipeline{
                     echo "healthcheck api_python"                    
                     #curl -v -k http://172.31.35.158:5000 | grep "HTTP/1.0"
                     API_TEST_RESULT = $(curl -s -o /dev/null -I -w "%{http_code}" http://172.31.35.158:5000)
-                    #echo "${API_TEST_RESULT}"
+                    echo "${API_TEST_RESULT}"
                 '''
+            }
+        } //fin stage post
+
+        stage("Test-api") {
+            agent {
+                label 'vm_host'
+            }
+            steps {
+                script{
+                    def COLLECTION_PATH = "https://api.postman.com/collections/17594112-e1ce6c25-32be-4d33-93e1-7f67675a104d?access_key=PMAT-01HFCAMEGKSP1Z3KJ1MKDQ07QC"
+                    sh '''
+                        newman run COLLECTION_PATH
+                    '''
+                }
             }
         } //fin stage post
         
